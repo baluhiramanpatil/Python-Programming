a1f327900ca73889e579c9e7d406d5b88cf21123
Balu Patil <105616999+baluhiramanpatil@users.noreply.github.com>
2022-06-28 10:55:06 +0530

    Add files via upload

---
+++ 08 membership operators.py
@@ -0,0 +1,24 @@
+# membership operators (in, not in)
+
+'''
+in (return true if a sequence with the specified value is present in the object)
+not in(return true if a sequence with the specified value is not present in the object)
+
+'''
+x = 'hello world'
+
+print('h' in x)
+print('p' in x)
+print('p' not in x)
+
+
+y = [10, 20, 30, 40, 50]
+
+print(10 in y)
+print(10 not in y)
+print(100 in y)
+print(100 not in y)
+print('h' in y)
+print('h' not in y)
+
+
