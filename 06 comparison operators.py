2dc58518d6bdc49bcb0ac327143fef445b75fe4e
Balu Patil <105616999+baluhiramanpatil@users.noreply.github.com>
2022-06-28 09:26:12 +0530

    Add files via upload

---
+++ 06 comparison operators.py
@@ -0,0 +1,21 @@
+# comparison operators (==, !=, >, <, >=, <=)
+
+'''
+== equal
+!= not equal
+> greater than
+< less than
+>= greater than or equal to
+<= less than or equal to
+
+'''
+
+x = 10
+y = 20
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
