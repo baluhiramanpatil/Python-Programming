8ab3f74fc18f0c96b910253710231b63fbe27aab
Balu Patil <105616999+baluhiramanpatil@users.noreply.github.com>
2022-06-29 10:35:31 +0530

    Add files via upload

---
+++ 09 identity operators.py
@@ -0,0 +1,12 @@
+# identity operators (is, is not)
+
+'''
+is (return true if both variables are the same object)
+is not (return true if both variables are not the same object)
+
+'''
+x = 10
+y = 10
+
+print(x is y, x == y)
+print(x is not y, x != y)
