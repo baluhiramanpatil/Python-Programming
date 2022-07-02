58404e441cc0200b91df5650a58fa7b551047ba2
Balu Patil <105616999+baluhiramanpatil@users.noreply.github.com>
2022-07-02 10:22:32 +0530

    Add files via upload

---
+++ 16 while loop.py
@@ -0,0 +1,20 @@
+# while loop
+
+i = 1
+
+while i <= 10:
+    print(i, "Thanks")
+    i = i + 1
+
+print(i)
+
+
+# reverse while loop
+
+i = 5
+
+while i >= 1:
+    print(i, "Thanks")
+    i = i - 1
+
+print(i)
