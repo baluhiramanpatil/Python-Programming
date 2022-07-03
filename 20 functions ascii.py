91c7b23c0b2213542c57c8dccbaf45b85caf1819
Balu Patil <105616999+baluhiramanpatil@users.noreply.github.com>
2022-07-03 09:14:12 +0530

    Add files via upload

---
+++ 20 functions ascii.py
@@ -0,0 +1,30 @@
+# functions ascii 
+'''
+chr() 
+
+- it takes integer and return a character string
+- convert integer 65 to ASCII character ('A')
+- example:
+    y = chr(65)
+    print(y, type(y))
+
+ord()
+
+- It takes a single character and return an integer
+- example:
+    y = ord('A')
+    print(y, type(y))
+
+'''
+# chr()
+
+a = 65
+print(chr(a))
+
+a = 66
+print(chr(a))
+
+# ord()
+
+y = ord('A')
+print(y, type(y))
